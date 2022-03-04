2dfb1fb26dd6deac3c3601cf819713cd739ddeb4
iplux <49649756+ipluxteamx@users.noreply.github.com>
2022-03-04 21:06:00 +0100

    load song lua shit lol

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -773,6 +773,20 @@ class FunkinLua {
 		Lua_helper.add_callback(lua, "getSongPosition", function() {
 			return Conductor.songPosition;
 		});
+					
+		Lua_helper.add_callback(lua, "loadSong", function(jsonInput:String, folder:String, skipTransition:Bool) {
+			trace("trying to load new song " + jsonInput + " from folder " + folder);
+			if(skipTransition)
+			{
+				FlxTransitionableState.skipNextTransIn = true;
+				FlxTransitionableState.skipNextTransOut = true;
+			}
+
+			PlayState.SONG = Song.loadFromJson(jsonInput, folder);
+			FlxG.sound.music.stop();
+			PlayState.cancelMusicFadeTween();
+			LoadingState.loadAndSwitchState(new PlayState());
+		});
 
 		Lua_helper.add_callback(lua, "getCharacterX", function(type:String) {
 			switch(type.toLowerCase()) {
