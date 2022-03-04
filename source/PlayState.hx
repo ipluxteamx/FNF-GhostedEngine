8deb99f716367370536c8d187f4fba90695e04d7
iplux <49649756+ipluxteamx@users.noreply.github.com>
2022-03-04 14:16:40 +0100

    oopsies it was already an option sorry

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2258,10 +2258,10 @@ class PlayState extends MusicBeatState
 
 	override public function update(elapsed:Float)
 	{
-		/*if (FlxG.keys.justPressed.NINE)
+		if (FlxG.keys.justPressed.NINE)
 		{
 			iconP1.swapOldIcon();
-		}*/
+		}
 
 		callOnLuas('onUpdate', [elapsed]);
 
