0c250775acd56ce0c738b86be66187074a5257bb
iplux <49649756+ipluxteamx@users.noreply.github.com>
2022-03-05 13:02:31 +0100

    screen resolution

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -28,6 +28,7 @@ class ClientPrefs {
 	public static var noReset:Bool = false;
 	public static var healthBarAlpha:Float = 1;
 	public static var controllerMode:Bool = false;
+	public static var screenRes:String = '1280x720';
 	public static var gameplaySettings:Map<String, Dynamic> = [
 		'scrollspeed' => 1.0,
 		'scrolltype' => 'multiplicative', 
@@ -195,6 +196,7 @@ class ClientPrefs {
 		FlxG.save.data.gameplaySettings = gameplaySettings;
 		FlxG.save.data.inputSystem = inputSystem;
 		FlxG.save.data.controllerMode = controllerMode;
+		FlxG.save.data.screenRes = screenRes;
 	
 		FlxG.save.flush();
 
@@ -297,6 +299,9 @@ class ClientPrefs {
 		}
 		if(FlxG.save.data.controllerMode != null) {
 			controllerMode = FlxG.save.data.controllerMode;
+		}
+		if(FlxG.save.data.screenRes != null) {
+			screenRes = FlxG.save.data.screenRes;
 		}
 		if(FlxG.save.data.gameplaySettings != null)
 		{
